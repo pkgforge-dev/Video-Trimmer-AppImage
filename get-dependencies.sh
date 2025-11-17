25de3b9d5cf2a2c5e36914272f315a419876d6e7
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-17 18:49:25 +0100

    Install `pipewire-jack`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,8 @@ ARCH=$(uname -m)
 
 echo "Installing package and its dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm video-trimmer
+pacman -Syu --noconfirm video-trimmer \
+                        pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
