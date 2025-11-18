86ff071507302669b6d39c4c29a5130c0ad24ac0
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-18 09:52:58 +0100

    Also deploy Vulkan, now that we're not using `gtk4-mini` anymore

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.gitlab.YaLTeR.VideoTrimmer.svg
 export DESKTOP=/usr/share/applications/org.gnome.gitlab.YaLTeR.VideoTrimmer.desktop
 export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 export DEPLOY_GSTREAMER=1
 export DEPLOY_PIPEWIRE=1
 export STARTUPWMCLASS=video-trimmer # For Wayland, this is 'org.gnome.gitlab.YaLTeR.VideoTrimmer', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
