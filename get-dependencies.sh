a74eda3712347b2377ebc6186a234116662167a0
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-14 00:56:40 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -21,4 +21,4 @@ fi
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano intel-media-driver-mini
+get-debloated-pkgs icu-mini libxml2-mini mesa-nano gdk-pixbuf2-mini librsvg-mini opus-mini ffmpeg-mini intel-media-driver-mini
